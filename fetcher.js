10d9df23f50f8ddfa90154b0ebece329096c8fad
bobby-brice <bobby.brice@gmail.com>
2020-06-04 01:49:55 +0000

    added header validation

--- fetcher.js
+++ fetcher.js
@@ -1,12 +1,15 @@
 const request = require('request');
 const fs = require('fs');
 const args = process.argv.slice(2);
-
-
-
+const {validateHeaderName} = require('http');
 
 //make a request through CLI (args)
 request(args[0], (error, response, body) => {
+  try {
+    validateHeaderName("");
+  } catch (err) {
+    err.message = "URL must be valid http token";
+  }
   //write the file to the path provided from args
   fs.exists(args[1], (exists) => {
     if (exists) {
@@ -21,9 +24,6 @@ request(args[0], (error, response, body) => {
       });
     }
   });
-
-
-
 });
 
 
